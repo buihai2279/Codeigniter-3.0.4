f7423e0c9cd69f8c486586d276a8a6b8c78c6912
buihai2279 <haibui2279@gmail.com>
2016-03-02 23:43:08 +0700

    2-3

--- application/controllers/manager_product.php
+++ application/controllers/manager_product.php
@@ -10,6 +10,25 @@ class Manager_product extends CI_Controller {
 		$this->load->view('manager_product/list');
 		$this->load->view('manager_user/footer');
 	}
+	public function test()
+	{
+		$this->load->library('simple_html_dom');
+		// 1. Write a function with parameter "$element"
+		function my_callback($element) {
+		    if ($element->tag=='input')
+		        $element->outertext = 'input';
+		    if ($element->tag=='img')
+		        $element->outertext = 'img';
+		    if ($element->tag=='a')
+		        $element->outertext = 'a';
+		}
+		// 2. create HTML Dom
+		$html = file_get_html('http://www.google.com/');
+		// 3. Register the callback function with it's function name
+		$html->set_callback('my_callback');
+		// 4. Callback function will be invoked while dumping
+		echo $html;
+	}
 
 }
 
